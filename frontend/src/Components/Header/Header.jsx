d2e127f3e583e904bb05af0f92a3a2c348652d31
anshulshakya0605 <anshulshakya0605@gmail.com>
2024-10-23 18:58:10 +0530

    Initial commit

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+          <h2>Order your favorite food here</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut velit sit itaque labore, autem eveniet sed, temporibus vitae blanditiis eum dolore minus quos voluptatibus consectetur necessitatibus sint explicabo, unde ratione?</p>
+          <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
